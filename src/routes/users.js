0446617135e9006cc281c5c3ad204c637a6cfb7e
cihat <cihat02020202@gmail.com>
2022-01-27 01:24:23 +0300

    Add socketServer for TODO

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,10 +1,15 @@
 const express = require('express')
 const User = require('../models/user')
+// const socketServer = require('../socket-connection')
 
 const router = express.Router()
 
 router.get('/', async (req, res) => {
   res.send(await User.find())
 })
+
+// router.get('/:room', (req, res) => {
+//   res.render('room', { title: 'Room Page', roomId: req.params.room })
+// })
 
 module.exports = router
