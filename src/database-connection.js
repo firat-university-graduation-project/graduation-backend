52dc98a6d964f55d3bf5eee29187b31acec8c7a4
cihat <cihat02020202@gmail.com>
2022-01-26 21:34:44 +0300

    Update local mongoDB url

--- src/database-connection.js
+++ src/database-connection.js
@@ -1,7 +1,8 @@
 /* eslint-disable no-console */
 const mongoose = require('mongoose')
 
-const connectionString = process.env.MONGODB_CONNECTION_STRING || 'mongodb://localhost'
+const connectionString =
+  process.env.MONGODB_CONNECTION_STRING || 'mongodb://localhost:27017/graduation'
 
 mongoose.set('debug', true)
 
