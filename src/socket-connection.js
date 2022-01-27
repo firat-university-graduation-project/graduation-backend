75fa8e5876b4da93704f60a1da24e490b808cd8f
cihat <cihat02020202@gmail.com>
2022-01-27 19:53:05 +0300

    Remove logs in socket actions

--- src/socket-connection.js
+++ src/socket-connection.js
@@ -9,9 +9,7 @@ module.exports = (app, server) => {
   socketServer.on('connection', socket => {
     socket.on('join-room', (roomId, userId) => {
       socket.join(roomId)
-      console.log('Join room ')
       socket.to(roomId).broadcast.emit('user-connected', userId)
-      console.log('User connected')
       socket.on('message', message => {
         // send message to the same room
         io.to(roomId).emit('createMessage', message)
@@ -19,7 +17,6 @@ module.exports = (app, server) => {
 
       socket.on('disconnect', () => {
         socket.to(roomId).broadcast.emit('user-disconnected', userId)
-        console.log('User disconnected')
       })
     })
   })
