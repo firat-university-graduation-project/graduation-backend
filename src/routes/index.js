b10a1ef51d1eb78f114717b2c6a56638699fee98
cihat <cihat02020202@gmail.com>
2022-02-13 11:23:15 +0300

    Update routes && Change template engine

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,9 @@
 const express = require('express')
-const { v4: uuidV4 } = require('uuid')
 
 const router = express.Router()
 
 router.get('/', (req, res) => {
-  res.redirect(`/${uuidV4()}`)
-})
-
-router.get('/:room', (req, res) => {
-  res.render('room', { roomId: req.params.room })
+  res.render('index')
 })
 
 router.get('/ping', (req, res) => {
